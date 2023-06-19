bdf61ef554aa9d6500919d6e8c526856a7827327
Musa Abdullahi <moosaabdullahi45@gmail.com>
2023-06-20 01:36:54 +0200

    new

--- clean/urls.py
+++ clean/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("about", AboutPageView.as_view(), name="about"),
     path("contact", ContactPageView.as_view(), name="contact"),
     path("services", ServicePageView.as_view(), name="services"),
-    path("training", TrainingPageView.as_view(), name="training"),
+    path("trainings", TrainingPageView.as_view(), name="trainings"),
     path("service/<str:service_slug>/", ServiceDetailPageView.as_view(), name="service"),
+    path("training/<str:training_slug>/", TrainingDetailPageView.as_view(), name="training"),
     # path("contact_form", ContactAPiView.as_view(), name="contact_form"),
 ]
