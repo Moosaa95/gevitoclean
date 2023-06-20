eab5c8ff20bc47583733cf3c38601c0b7da13ef5
Musa Abdullahi <moosaabdullahi45@gmail.com>
2023-06-20 11:06:36 +0200

    new

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -56,7 +56,60 @@
 		});
 
 	};
-	carousel();
+	// carousel();
+
+	new Swiper(".slides-1", {
+		speed: 200,
+		loop: true,
+		autoplay: {
+		  delay: 5000,
+		  disableOnInteraction: false,
+		},
+		slidesPerView: "auto",
+		pagination: {
+		  el: ".swiper-pagination",
+		  type: "bullets",
+		  clickable: true,
+		},
+		navigation: {
+		  nextEl: ".swiper-button-next",
+		  prevEl: ".swiper-button-prev",
+		},
+	  });
+	
+	  /**
+	   * Init swiper slider with 2 slides at once in desktop view
+	   */
+	  new Swiper(".slides-2", {
+		speed: 200,
+		loop: true,
+		autoplay: {
+		  delay: 5000,
+		  disableOnInteraction: false,
+		},
+		slidesPerView: "auto",
+		pagination: {
+		  el: ".swiper-pagination",
+		  type: "bullets",
+		  clickable: true,
+		},
+		navigation: {
+		  nextEl: ".swiper-button-next",
+		  prevEl: ".swiper-button-prev",
+		},
+		breakpoints: {
+		  320: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		  },
+	
+		  1200: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		  },
+		},
+	  });
+	
 
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
@@ -175,6 +228,8 @@
 		} , { offset: '95%' } );
 	};
 	contentWayPoint();
+
+	
 	
 
 
